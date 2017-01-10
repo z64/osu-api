67add6c1910412c92eadf66c3b2eebcbb9d71e26
z64 <zachnowicki@gmail.com>
2017-01-10 00:38:57 -0500

    add Client

---
+++ lib/osu/client.rb
@@ -0,0 +1,12 @@
+module Osu
+  # Client interface that caches a token to make
+  # subsequent requests with
+  class Client
+    # @return [String] auth key
+    attr_reader :key
+
+    def initialize(key)
+      @key = key
+    end
+  end
+end
