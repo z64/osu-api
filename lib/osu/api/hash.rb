628c4a9410c94f291c7a6cd4b5997b80461b69ee
z64 <zachnowicki@gmail.com>
2017-01-10 01:45:13 -0500

    add api/hash.rb

---
+++ lib/osu/api/hash.rb
@@ -0,0 +1,27 @@
+# This file is a series of helpers to compose
+# querystrings to the osu API
+module Osu
+  module API
+
+    module_function
+
+    # @return [Hash] user hash part
+    def User(data)
+      if data.is_a? Integer
+        { u: data, type: 'id' }
+      elsif data.is_a? String
+        { u: data, type: 'string' }
+      end
+    end
+
+    # @return [Hash] mode hash part
+    def Mode(mode)
+      { m: MODE.index(mode) }
+    end
+
+    # @return [Hash] auth hash part
+    def Auth(auth)
+      { k: auth }
+    end
+  end
+end
