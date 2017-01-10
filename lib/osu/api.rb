6bb79917814fcfec17a30a21061d9af7900f30e2
z64 <zachnowicki@gmail.com>
2017-01-10 01:06:26 -0500

    add a generic GetRequest module

--- lib/osu/api.rb
+++ lib/osu/api.rb
@@ -1,8 +1,25 @@
+require 'json'
 require 'rest-client'
 
 module Osu
   # Interface to Osu's REST API
   module API
     API_URL = 'https://osu.ppy.sh/api'.freeze
+
+    # Structure of a GET request to the API
+    module GetRequest
+      # @return [String] endpoint to query
+      attr_reader :endpoint
+
+      # @return [Hash] parameters to pass as a querystring
+      attr_reader :params
+
+      def execute(key)
+        params.merge! { k: key }
+        
+        response = RestClient.get endpoint, params: params
+        JSON.parse response
+      end
+    end
   end
 end
