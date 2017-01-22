ac5774a5f3d2cdf653580d698384b7aea0ba20be
z64 <zachnowicki@gmail.com>
2017-01-22 02:10:08 -0500

    add a GetRequest for BeatmapScore

--- lib/osu/api.rb
+++ lib/osu/api.rb
@@ -4,6 +4,7 @@ require 'rest-client'
 require 'osu/api/constants'
 require 'osu/api/hash'
 require 'osu/api/get_request'
+require 'osu/api/mods.rb'
 
 module Osu
   # Interface to Osu's REST API
@@ -32,6 +33,25 @@ module Osu
         @params.merge! API.beatmap_set(set) if set
         @params.merge! API.user(author) if author
         @params.merge! API.mode(mode) if mode
+        @params.merge! API.limit(limit) if limit
+      end
+    end
+
+    class BeatmapScore
+      include GetRequest
+
+      def initialize(id, user: nil, mode: nil, mods: nil, limit: nil)
+        @endpoint = 'get_scores'
+
+        @params = {}
+
+        @params.merge! API.beatmap(id) if id
+        @params.merge! API.user(user) if user
+        @params.merge! API.mode(mode) if mode
+        if mods
+          mods = API::Mods.bits(mods) if mods.is_a? Array
+          @params.merge! API.mods(mods)
+        end
         @params.merge! API.limit(limit) if limit
       end
     end
